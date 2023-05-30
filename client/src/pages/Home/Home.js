89fa36b8ca3ef4f610b494d14354c7c758e40af7
mzahra976 <moradzade976@gmail.com>
2023-05-30 15:40:52 +0330

    change .gitignore

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,6 +1,5 @@
 import Slider from "../../components/Slider/Slider"
 import FeatureProducts from "../../components/FeatureProducts/FeatureProducts"
-// import FeatureProducts2 from "../../components/FeatureProducts/FeatureProducts2"
 import Categories from "../../components/Categories/Categories"
 import Contact from "../../components/Contact/Contact"
 import "./Home.scss"
